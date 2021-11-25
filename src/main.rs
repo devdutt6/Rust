ca5dc6bff068ab410f827ca3a7fdd88f65e1d011
devdutt-pedalsup <devdutt@pedalsup.com>
2021-11-25 18:57:14 +0530

    started with Rust

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// mod print;
+// mod types;
+// mod string1;
+// mod tuple;
+// mod arrays;
+// mod vectors;
+mod conditional;
+
+fn main() {
+    // print::print();
+    // types::run();
+    // string1::run();
+    // tuple::run();
+    // arrays::run();
+    // vectors::run();
+    conditional::run();
+}
